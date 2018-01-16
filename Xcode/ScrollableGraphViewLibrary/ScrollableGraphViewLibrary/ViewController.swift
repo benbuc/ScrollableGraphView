b7aae1fc89016f8a1cc208fb9864206a042e0ab7
Benito Buchheim <benito.buchheim@student.hpi.de>
2018-01-16 14:30:57 +0100

    added Xcode project for zooming

---
+++ Xcode/ScrollableGraphViewLibrary/ScrollableGraphViewLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollableGraphViewLibrary
+//
+//  Created by Benito Buchheim on 16.01.18.
+//  Copyright © 2018 mission eins UG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
